849b281a11a892806b35a153b2806dcb4e7e4ff4
genericcereal <jgoggin1@gmail.com>
2025-06-19 07:38:53 -0600

    cleanup

--- test_app.sh
+++
@@ -1,9 +0,0 @@
-#\!/bin/bash
-# Run the app and simulate frame creation
-./cubit-quick.app/Contents/MacOS/cubit-quick 2>&1 &
-APP_PID=$\!
-sleep 3
-# Wait a bit then kill the app
-sleep 10
-kill $APP_PID 2>/dev/null
-wait $APP_PID 2>/dev/null
